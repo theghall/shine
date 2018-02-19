0b2423039ed78bf5eb12374c4639ad946b714d64
Gary Hall <guategary@gmail.com>
2018-02-19 12:01:20 -0500

    Add email constraint to postgresql

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Shine
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.active_record_schema_format = :sql
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219121215) do
+ActiveRecord::Schema.define(version: 20180219165413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
